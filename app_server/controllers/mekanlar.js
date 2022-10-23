509a837ad21bb323691f96ac7025a07bbe29b5e6
burakgenel <burakfarukgenel99@gmail.com>
2022-10-23 20:51:00 +0300

    odev3

--- app_server/controllers/mekanlar.js
+++ app_server/controllers/mekanlar.js
@@ -1,21 +1,79 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
+const anaSayfa = function (req, res) {
+  res.render("anasayfa", {
+    "baslik": "Ana Sayfa",
+    "sayfaBaslik": {
+      "siteAd": "MekanBul",
+      "slogan": "Civardaki Mekanları Keşfet!",
+    },
+    "mekanlar": [
+      {
+        "ad": "Starbucks",
+        "adres": "Centrum Garden",
+        "puan": "4",
+        "imkanlar": ["Dünya Kahveleri", "Yiyecek", "Hızlı Wifi Bağlantısı"],
+        "mesafe": "100m",
+      },
+      {
+        "ad": "Gloria Jeans",
+        "adres": "Sdü Doğu Kampüsü",
+        "puan": "3",
+        "imkanlar": ["Kahve", "Çay", "Pasta"],
+        "mesafe": "5km",
+      },
+    ],
+  });
+};
 
-const anaSayfa = function (req, res, next) {
-    res.render('anasayfa', { title: 'Ana Sayfa'});
-}
-
-const mekanBilgisi = function (req, res, next) {
-    res.render('mekanbilgisi', { title: 'Mekan Bilgisi'});
-}
+const mekanBilgisi = function (req, res) {
+  res.render("mekanbilgisi", 
+  { 
+    "baslik": "Mekan Bilgisi",
+    "mekanBaslik": "Starbucks",
+    "mekanDetay": {
+        "ad": "Starbucks",
+        "adres": "Centrum Garden AVM",
+        "puan": "4",
+        "imkanlar": ["Dünya Kahveleri", "Kekler", "Pastalar"],
+        "koordinatlar": {
+            "enlem": "37.7",
+            "boylam": "30.5"
+        },
+        "saatler": [
+            {
+                "gunler": "Pazartesi-Cuma",
+                "acilis": "9:00",
+                "kapanis": "23:00",
+                "kapali": false
+            },
+            {
+                "gunler": "Cumartesi-Pazar",
+                "acilis": "10:00",
+                "kapanis": "22:00",
+                "kapali": false
+            }
+        ],
+        "yorumlar": [
+            {
+                "yorumYapan": "Burak Faruk Genel",
+                "puan": "4",
+                "tarih": "20 Ekim 2022",
+                "yorumMetni": "Kahveler iyi."
+            }
+        ]
+    }
+  
+ });
+};
 
 const yorumEkle = function (req, res, next) {
-    res.render('yorumekle', { title: 'Yorum Ekle'});
-}
+  res.render("yorumekle", { title: "Yorum Ekle" });
+};
 
-module.exports ={
-    anaSayfa,
-    mekanBilgisi,
-    yorumEkle
-}
+module.exports = {
+  anaSayfa,
+  mekanBilgisi,
+  yorumEkle,
+};
